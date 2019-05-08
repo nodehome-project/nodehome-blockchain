fda20d8fbf9808fff0c99676d161be96e4ce685c
nodehome-project <48472782+nodehome-project@users.noreply.github.com>
2019-05-08 18:32:58 +0900

    Update check-blockchain.sh
    setup

--- check-blockchain.sh
+++ check-blockchain.sh
@@ -2,20 +2,4 @@
 #
 # Copyright goldblock All Rights Reserved
 
-function CheckNetwork() {
-  set +e
-  DOCKER_NETWORK_IDS=$(docker network ls | grep 'blockchain-net' | awk '{print $2}')
-  if [ -z "$DOCKER_NETWORK_IDS" -o "$DOCKER_NETWORK_IDS" == " " ]; then
-    docker network create --attachable --driver bridge 'blockchain-net'
-  fi
-  set -e
-}
-
-if [ "$1" == "start" ]; then
-  CheckNetwork
-  docker-compose -f docker-compose.yml up -d
-elif [ "$1" == "stop" ]; then
-  docker-compose -f docker-compose.yml down
-else
-  echo "Usage : ./nodehome-blockchain.sh start/stop"
-fi
+curl --header "Content-Type: application/json" --request POST --data '{"chaincode":"ecchain","query_type":"query","func_args":["PID","10000"],"func_name":"version"}' http://127.0.0.1:8050/chaincode_query
