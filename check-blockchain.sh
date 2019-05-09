f8db2831723dd505ad6b2779f87b43916a64e370
nodehome-project <48472782+nodehome-project@users.noreply.github.com>
2019-05-09 11:31:52 +0900

    add --silent option

--- check-blockchain.sh
+++ check-blockchain.sh
@@ -3,4 +3,4 @@
 # Copyright goldblock All Rights Reserved
 
 # Query to blockchain network
-curl --header "Content-Type: application/json" --request POST --data '{"chaincode":"ecchain","query_type":"query","func_args":["PID","10000"],"func_name":"version"}' http://127.0.0.1:8050/chaincode_query
+curl --silent --header "Content-Type: application/json" --request POST --data '{"chaincode":"ecchain","query_type":"query","func_args":["PID","10000"],"func_name":"version"}' http://127.0.0.1:8050/chaincode_query
